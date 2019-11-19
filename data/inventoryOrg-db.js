e5e61f502da5be3ab75163c91d9f877971803300
Marianna <mckd2018@gmail.com>
2019-11-19 11:26:11 -0800

    [upd] a delete comment to test Travis CI after adding yml file

--- data/inventoryOrg-db.js
+++ data/inventoryOrg-db.js
@@ -2,7 +2,6 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-/* ODM */
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/inventoryOrg-db',
